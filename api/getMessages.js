f4a9e1b5a0dca359f8b09a140453d9a32f3be9ec
DaffTzy5912 <dafanation999@gmail.com>
2025-05-01 21:34:16 +0700

    Update getMessages.js

--- api/getMessages.js
+++ api/getMessages.js
@@ -1,11 +1,12 @@
 import fs from 'fs/promises';
 
 export default async function handler(req, res) {
+  const { room } = req.query;
   try {
-    const data = await fs.readFile('data/messages.json', 'utf8');
+    const data = await fs.readFile(`data/${room}.json`, 'utf8');
     const json = JSON.parse(data);
-    res.status(200).json({ messages: json.messages || [] });
-  } catch (err) {
-    res.status(500).json({ messages: [] });
+    res.status(200).json({ messages: json.messages });
+  } catch {
+    res.status(200).json({ messages: [] });
   }
 }
